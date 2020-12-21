78d409e3e31d5911c26c898bcbbfc353b0a5b3bf
Meehir Patel <meehir_patel@hotmail.co.uk>
2020-12-21 22:04:05 +0000

    add friend feature

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,6 +2,8 @@ class FriendshipsController < ApplicationController
     def create
         @user = current_user
         friend = User.find_by(params[:id])
-        @user.friend_request(User.find_by(id: params[:id]))
+        @user.friend_request(friend)
+        
+        redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show] do
-    resources :friendships, only: [:create]
-  end
+  resources :users, only: [:show]
+  resources :friendships, only: [:create]
+
 
 
 end
